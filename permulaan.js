114a1285a20061e7d23d85e7d193004dbf4f0d5a
bag-us <bagustwo5@gmail.com>
2022-04-03 18:13:17 +0700

    first commit

---
+++ permulaan.js
@@ -0,0 +1,7 @@
+var produk1 = "Modem";
+var produk2 = "Hardisk";
+var produk3 = "Flashdisk";
+var makanan = ["Nasi Goreng", "Mie Ayam", "Mie Gelas"];
+for(let i=0; i < makanan.length; i++){
+    console.log(makanan[i]);
+}
